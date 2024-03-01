4ff4f0444ea65d085218d0d686c43b0bbbee01a9
deku450harsh <gargn9039@gmail.com>
2024-03-01 19:52:37 +0530

    added typing animation

--- chat_application_client/src/Components/chatArea/ChatArea.jsx
+++ chat_application_client/src/Components/chatArea/ChatArea.jsx
@@ -14,32 +14,52 @@ import { myContext } from "../Main/MainContainer";
 import io from "socket.io-client";
 import { toast } from "react-toastify";
 import image from "../../assests/doodle.jpg";
+import Lottie from "react-lottie";
 import SeeUsers from "../SeeUsers.jsx";
+import animationData from "../../assests/Animation - 1709298838619.json";
 
-var socket , selectedChat;
+var socket, selectedChat;
 const ENDPOINT = `${process.env.REACT_APP_DEPLOYMENT_URL}`;
 
 const ChatArea = () => {
   // const lightTheme = useSelector((state) => state.themeKey);
-  const { refresh, setRefresh ,notification , setNotification ,chatcontext , setChatcontext} = useContext(myContext);
+  const {
+    refresh,
+    setRefresh,
+    notification,
+    setNotification,
+    chatcontext,
+    setChatcontext,
+  } = useContext(myContext);
   const [messageContent, setMessageContent] = useState("");
   const messagesEndRef = useRef(null);
   const dyParams = useParams();
-  const [chat_id, chat_user,isGroupChat, avatarImage] = dyParams._id.split("&");
+  const [chat_id, chat_user, isGroupChat, avatarImage] =
+    dyParams._id.split("&");
   const userData = JSON.parse(Cookies.get("userData"));
   const [allMessages, setAllMessages] = useState([]);
   const [loaded, setloaded] = useState(false);
   const [socketConnectionStatus, setSocketConnectionStatus] = useState(false);
   const lighttheme = useSelector((state) => state.themeKey);
+  const [istyping, setIstyping] = useState(false);
+  const [typing, setTyping] = useState(false);
   const msgRef = useRef(null);
-  const [open,setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
   // const [allMessagesCopy, setAllMessagesCopy] = useState([]);
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   const fetchMessages = async () => {
     if (!chatcontext && !chat_id) {
-      console.log("chatcontext is not available",chatcontext)
+      console.log("chatcontext is not available", chat_id);
       return;
     }
     try {
@@ -56,13 +76,15 @@ const ChatArea = () => {
       setAllMessages(data);
       setloaded(true);
       socket.emit("join chat", chat_id);
-    }catch(error) {
+    } catch (error) {
       setloaded(true);
       toast.error("Error while fetching");
     }
   };
 
   const sendMessage = async () => {
+    if (!messageContent.trim()) return;
+ 
     const config = {
       headers: {
         Authorization: `Bearer ${userData.data.token}`,
@@ -75,11 +97,12 @@ const ChatArea = () => {
       `${process.env.REACT_APP_DEPLOYMENT_URL}/message/`,
       {
         content: messageContent,
-        chatId: chatcontext || chat_id,
+        chatId: chat_id,
       },
       config
     );
     socket.emit("new message", data);
+    socket.emit("stop typing", chat_id);
     setRefresh((prevRefresh) => !prevRefresh);
     setAllMessages([...allMessages, data]);
   };
@@ -90,53 +113,50 @@ const ChatArea = () => {
     socket.on("connected", () => {
       setSocketConnectionStatus(true);
     });
+    socket.on("typing",()=>setTyping(true));
+    socket.on("stop typing",()=>setTyping(false));
   }, []);
 
-
   useEffect(() => {
+    selectedChat = chat_id;
     fetchMessages();
-    selectedChat = chatcontext;
-  }, [chatcontext,userData.data.token]);
- 
+  }, [chat_id]);
 
   useEffect(() => {
     socket.on("message recieved", (newMessage) => {
-      if (!selectedChat || selectedChat._id !== newMessage.chat._id) {
-        const present = notification.find((f)=>f.chat._id === newMessage.chat._id);
-        if(!present){
-          setNotification([newMessage,...notification]);
+      if (!selectedChat || selectedChat !== newMessage.chat._id) {
+        const present = notification.find(
+          (f) => f.chat._id === newMessage.chat._id
+        );
+        if (!present) {
+          setNotification([newMessage, ...notification]);
           setRefresh(!refresh);
+          toast(`New Message from ${newMessage.sender.name}!`, {
+            position: "bottom-left",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
         }
         console.log("Users are not chating with each other");
       } else {
         console.log("message is recieved");
-        setAllMessages([...allMessages, newMessage]);
         setRefresh(!refresh);
+        setAllMessages((prevMessages) => [...prevMessages, newMessage]);
+        console.log(allMessages);
       }
     });
+    return () => {
+      if (socket) {
+        console.log("Offed message recived");
+        socket.off("message recieved");
+      }
+    };
   });
-
-
-
-  // useEffect(() => {
-  //   console.log("Users refreshed");
-  //   const config = {
-  //     headers: {
-  //       Authorization: `Bearer ${userData.data.token}`,
-  //     },
-  //   };
-  //   axios.get("http://localhost:5000/message/" + chat_id, config)
-  //     .then(({ data }) => {
-  //       // console.log("Hii i am allmessagescopy");
-  //       setAllMessages(data);
-  //       // console.log(data);
-  //       // console.log("hii ba")
-  //       setloaded(true);
-  //       socket.emit("join chat",chat_id);
-  //     });
-  // }, [refresh,chat_id, userData.data.token]);
-
-
 
   useEffect(() => {
     if (msgRef.current) {
@@ -144,14 +164,37 @@ const ChatArea = () => {
     }
   });
 
-  const handleOpen = ()=>{
+  const handleOpen = () => {
     setOpen(true);
-  }
-  const handleclose = ()=>{
+  };
+  const handleclose = () => {
     setOpen(false);
+  };
+
+  const typingHandler =(e)=>{
+    setMessageContent(e.target.value);
+    if(!socketConnectionStatus){
+      return;
+    }
+
+    if(!istyping){
+      setIstyping(true);
+      socket.emit("typing",chat_id);
+    }
+
+    let lastTypingTime = new Date().getTime();
+    var timerLength = 3000;
+
+    setTimeout(() => {
+      var timeNow = new Date().getTime();
+      var timeDiff = timeNow - lastTypingTime;
+      if (timeDiff >= timerLength && istyping) {
+        socket.emit("stop typing", chat_id);
+        setIstyping(false);
+      }
+    }, timerLength);
+
   }
-
-
 
   if (!loaded) {
     return (
@@ -201,8 +244,16 @@ const ChatArea = () => {
             </p>
             {/* <p className="con-timestamp">{props.timestamp}</p> */}
           </div>
-          {isGroupChat=== "true" ? <SeeUsers handleOpen={handleOpen} handleClose={handleclose} open={open} name={chat_user} /> : <div/>}
-          
+          {isGroupChat === "true" ? (
+            <SeeUsers
+              handleOpen={handleOpen}
+              handleClose={handleclose}
+              open={open}
+              name={chat_user}
+            />
+          ) : (
+            <div />
+          )}
         </div>
         <div className="messages-container" ref={msgRef}>
           <div
@@ -222,15 +273,33 @@ const ChatArea = () => {
             })}
         </div>
         <div ref={messagesEndRef} className="BOTTOM" />
+        {typing ? (
+          <div style={{display:"flex" , padding:"0px 10px"}}>
+            <div className="message-other-icon">
+              <img
+                src={`data:image/svg+xml;base64,${avatarImage}`}
+                alt=""
+                width={"40px"}
+                height={"40px"}
+              />
+            </div>
+            <Lottie
+              options={defaultOptions}
+              height={40}
+              width={70}
+              style={{ marginBottom: 10, marginLeft: 10 }}
+            />
+          </div>
+        ) : (
+          <></>
+        )}
         <div className={"text-input-area " + (lighttheme ? "" : "darker")}>
           <input
             type="text"
             placeholder="Type a Message"
             className={"search-box " + (lighttheme ? "" : "darker")}
             value={messageContent}
-            onChange={(e) => {
-              setMessageContent(e.target.value);
-            }}
+            onChange={typingHandler}
             onKeyDown={(event) => {
               if (event.code == "Enter") {
                 // console.log(event);
--- chat_application_server/index.js
+++ chat_application_server/index.js
@@ -1,82 +1,87 @@
-const express = require('express');
+const express = require("express");
 const dotenv = require("dotenv");
-const mongoose = require('mongoose');
-const userRoutes = require("./routes/userRoutes")
-const chatRoutes = require("./routes/chatRoutes")
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-const cors = require('cors');
-const { Server, Socket } = require('socket.io');
-const Message = require('./models/message');
-const Chat = require('./models/chat');
-
-
+const cors = require("cors");
+const { Server, Socket } = require("socket.io");
+const Message = require("./models/message");
+const Chat = require("./models/chat");
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin:"*",
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/',(req,res)=>{
-    res.send("Hello");
-})
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
-app.use("/user",userRoutes);
-app.use("/chat",chatRoutes);
-app.use("/message",messageRoutes);
+app.use("/user", userRoutes);
+app.use("/chat", chatRoutes);
+app.use("/message", messageRoutes);
 
-const connectDb = async()=>{
-    try{
-        const connect = await mongoose.connect(process.env.MONGO_URL)
-        // const result = await Message.deleteMany({});
-        // const result = await Chat.deleteMany({});
-        console.log("database connected successfully")
-    }
-    catch(error){
-        console.log("Error connecting to dataBase");
-        console.log(error);
-    }
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URL);
+    // const result = await Message.deleteMany({});
+    // const result = await Chat.deleteMany({});
+    console.log("database connected successfully");
+  } catch (error) {
+    console.log("Error connecting to dataBase");
+    console.log(error);
+  }
 };
 connectDb();
 
-const server = app.listen(PORT,()=>{
-    console.log(`server is running on "http://localhost:${PORT}"`);
+const server = app.listen(PORT, () => {
+  console.log(`server is running on "http://localhost:${PORT}"`);
 });
 
-const io = require("socket.io")(server,{
-    pingTimeout:120000,
-    cors:{
-        origin:"*",
-        credentials:true
-    },
-    pingTimeout : 60000
+const io = require("socket.io")(server, {
+  pingTimeout: 120000,
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+  pingTimeout: 60000,
 });
 
-io.on("connection",(socket)=>{
-    socket.on("setup",(user)=>{
-        socket.join(user.data._id);
-        socket.emit("connected");
-    });
-    
-    socket.on("join chat",(room)=>{
-        console.log("user is joined");
-        console.log(room);
-        socket.join(room);
-    });
+io.on("connection", (socket) => {
+  socket.on("setup", (user) => {
+    socket.join(user.data._id);
+    socket.emit("connected");
+  });
 
-    socket.on("new message",(newMessageStatus)=>{
-        var chat = newMessageStatus.chat;
-        // console.log(chat);
-        if(!chat.users){
-            return console.log("chat users not defined")
-        }
-        chat.users.forEach((user) => {
-            if(user._id == newMessageStatus.sender._id) return;
-            socket.in(user._id).emit("message recieved", newMessageStatus)
-        });
-    })
-})
+  socket.on("join chat", (room) => {
+    console.log("user is joined");
+    console.log(room);
+    socket.join(room);
+  });
+
+  socket.on("typing",(room)=>{
+    socket.to(room).emit("typing")
+    console.log(room,"hii room")
+});
+  socket.on("stop typing",(room)=>socket.to(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageStatus) => {
+    var chat = newMessageStatus.chat;
+    // console.log(chat);
+    if (!chat || !chat.users) {
+      return console.log("chat users not defined");
+    }
+    chat.users.forEach((user) => {
+      if (user._id === newMessageStatus.sender._id) return;
+      socket.in(user._id).emit("message recieved", newMessageStatus);
+    });
+  });
+});
